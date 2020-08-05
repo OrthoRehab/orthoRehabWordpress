170eca552f948e0281cd3309cd48acaa3415d8a0
David Mitchell <davidm@orthorehab.com>
2020-08-05 11:51:51 -0600

    changed theme name and removed twitter handle on default code snippet

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 </div><!-- /.container -->
 
 <footer class="blog-footer">
-    <p>Template built for <a href="http://orthorehab.com">OrthoRehab</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    <p>Template built for <a href="http://orthorehab.com">OrthoRehab</a></p>
     <p><a href="#">Back to top</a></p>
 </footer>
 <?php wp_footer(); ?>
