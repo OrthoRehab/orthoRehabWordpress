fa7599e905f4fd5cbaefdba10b72a1f31d48270b
David Mitchell <davidm@orthorehab.com>
2020-08-05 14:06:28 -0600

    distiguished sidebar code in sidebar.php and functions.php

--- functions.php
+++ functions.php
@@ -34,10 +34,25 @@ function bootstrapstarter_widgets_init() {
         'after_title'   => '</h4>',
     ) );
 
+    register_sidebar( array(
+        'name'          => 'Sidebar - Inset',
+        'id'            => 'sidebar-1',
+        'before_widget' => '<div class="sidebar-module sidebar-module-inset">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => 'Sidebar - Default',
+        'id'            => 'sidebar-2',
+        'before_widget' => '<div class="sidebar-module">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>',
+    ) );
+
 }
 add_action( 'widgets_init', 'bootstrapstarter_widgets_init' );
-
-
-
 
 ?>
