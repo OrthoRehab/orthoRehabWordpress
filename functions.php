7c1ba3176029be2891ff0943ec39d18670e34eaa
David Mitchell <davidm@orthorehab.com>
2020-08-06 11:03:56 -0600

    modified functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
 
 function bootstrapstarter_enqueue_styles() {
-    wp_register_style('bootstrap', get_template_directory_uri() . 'css/bootstrap.min.css' );
+    wp_register_style('bootstrap', get_template_directory_uri() . 'bootstrap/css/bootstrap.min.css' );
     $dependencies = array('bootstrap');
     wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies );
 }
 
 function bootstrapstarter_enqueue_scripts() {
     $dependencies = array('jquery');
-    wp_enqueue_script('bootstrap', get_template_directory_uri().'js/bootstrap.min.js', $dependencies, '3.3.6', true );
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'bootstrap/js/bootstrap.min.js', $dependencies, '3.3.6', true );
 }
 add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_styles' );
 add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_scripts' );
